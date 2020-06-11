3136e310394fcfb17380e294683d065b10ed110d
Ujjwal Ojha <bring2uo@gmail.com>
2020-06-11 11:49:51 +0545

    Export MinLocalDate validator decorator

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './lib/validators/is-local-date';
 export * from './lib/validators/is-local-time';
 export * from './lib/validators/is-local-date-time';
+export * from './lib/validators/min-local-date';
 export * from './lib/properties/local-date.property';
 export * from './lib/properties/local-time.property';
 export * from './lib/properties/local-date-time.property';
